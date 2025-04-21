5708f16204c454c4517c4877f5ee2537c4ba6f9e
achano <hyuga.workplace@gmail.com>
2025-04-21 20:40:52 +0900

    設定修正と不要箇所削除

--- src/app/api/tasks/expired/route.ts
+++ src/app/api/tasks/expired/route.ts
@@ -5,11 +5,9 @@ import { NextResponse } from "next/server"
 export const GET = async () => {
   const currentDate = new Date().toLocaleDateString('ja-JP', {year: 'numeric', month: '2-digit', day: '2-digit'})
   .replace(/\//g, '-');
-  console.log(currentDate)
   try {
     await connectDB();
     const expiredTasks: TaskDocument[] = await TaskModel.find({isCompleted: false, dueDate: {$lt: currentDate}});
-    console.log(expiredTasks)
     return NextResponse.json({message: 'タスク取得成功', tasks: expiredTasks});
   } catch (error) {
     return NextResponse.json({message: 'タスク取得に失敗しました'}, {status: 500});
--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -4,7 +4,6 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI || '');
   } catch (error) {
-    console.log('Error connecting to MongoDB', error);
     throw new Error('Error connecting to MongoDB');
   }
 }
